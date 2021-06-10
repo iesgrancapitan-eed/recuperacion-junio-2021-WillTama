30a70240b99995a1aeca588e41b0be389df69d06
WillTama <hetagui95@gmail.com>
2021-06-10 21:15:05 +0200

    docs: eliminación carpeta docs

--- docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.eed.RA4.javadoc","l":"Contador"},{"p":"com.eed.RA4.javadoc","l":"Dado"},{"p":"com.eed.RA4.javadoc","l":"JuegoTest"},{"p":"com.eed.RA4.javadoc","l":"Jugador"},{"p":"com.eed.RA4.javadoc","l":"Tablero"}]
